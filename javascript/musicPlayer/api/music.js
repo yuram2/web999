445069d3d58008e2c5afe05c173864bf9ebdfb8d
yuram2 <lur0872@gmail.com>
2022-04-07 19:51:16 +0900

    20220407

---
+++ javascript/musicPlayer/api/music.js
@@ -0,0 +1,123 @@
+let allMusic = [
+    {
+        name : "봄여름가을겨울",
+        artist : "BIGBANG",
+        img : "music-1",
+        audio : "music-1"
+    },
+    {
+        name : "A Year Ago (Instrumental)",
+        artist : "NEFFEX",
+        img : "music-2",
+        audio : "music-2"
+    },
+    {
+        name : "Bite Me (Instrumental)",
+        artist : "NEFFEX",
+        img : "music-3",
+        audio : "music-3"
+    },
+    {
+        name : "Campfire",
+        artist : "Telecasted",
+        img : "music-4",
+        audio : "music-4"
+    },
+    {
+        name : "Dreaming On (Instrumental)",
+        artist : "NEFFEX",
+        img : "music-5",
+        audio : "music-5"
+    },
+    {
+        name : "Seasons",
+        artist : "Telecasted",
+        img : "music-6",
+        audio : "music-6"
+    },
+    {
+        name : "Something You Could Never Own (Instrumental)",
+        artist : "NEFFEX",
+        img : "music-7",
+        audio : "music-7"
+    },
+    {
+        name : "Tell Me That I Can't (Instrumental)",
+        artist : "NEFFEX",
+        img : "music-8",
+        audio : "music-8"
+    },
+    {
+        name : "The Itch (Instrumental)",
+        artist : "NEFFEX",
+        img : "music-9",
+        audio : "music-9"
+    },
+    {
+        name : "Til I Hear'em Say (Instrumental)",
+        artist : "NEFFEX",
+        img : "music-10",
+        audio : "music-10"
+    },
+    {
+        name : "걷고 싶다",
+        artist : "김종국",
+        img : "music-11",
+        audio : "music-11"
+    },
+    {
+        name : "선물",
+        artist : "나비",
+        img : "music-12",
+        audio : "music-12"
+    },
+    {
+        name : "열애중",
+        artist : "벤",
+        img : "music-13",
+        audio : "music-13"
+    },
+    {
+        name : "💖프로 이별러의 절절한 사랑고백💖",
+        artist : "양다일",
+        img : "music-14",
+        audio : "music-14"
+    },
+    {
+        name : "사랑해서 그래",
+        artist : "강민경",
+        img : "music-15",
+        audio : "music-15"
+    },
+    {
+        name : "늦은 밤 너의 집 앞 골목길에서",
+        artist : "노을",
+        img : "music-16",
+        audio : "music-16"
+    },
+    {
+        name : "여행",
+        artist : "볼빨간사춘기",
+        img : "music-17",
+        audio : "music-17"
+    },
+    {
+        name : "널 사랑하지 않아",
+        artist : "어반자카파",
+        img : "music-18",
+        audio : "music-18"
+    },
+    {
+        name : "고칠게",
+        artist : "진원",
+        img : "music-19",
+        audio : "music-19"
+    },
+    {
+        name : "GANADARA",
+        artist : "Jay Park (Feat. IU)",
+        img : "music-20",
+        audio : "music-20"
+    },
+    
+]
